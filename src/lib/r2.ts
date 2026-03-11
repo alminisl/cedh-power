544e45abcfc4231ff2cfe52cb889e0a52074d7fa
alminisl <almin.islamovic@gmail.com>
2026-03-11 11:58:00 +0100

    update

--- src/lib/r2.ts
+++ src/lib/r2.ts
@@ -2,7 +2,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 
 export const r2 = new S3Client({
   region: "auto",
-  endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  endpoint: "https://558c17f253ea8f90ae12f935ba040cc6.r2.cloudflarestorage.com",
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY_ID!,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
